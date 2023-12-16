83e57a32e675c7565e15f0b395e59110d28bbc62
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-16 23:29:22 +0300

    complete projectservices

--- frontend/src/features/Projects/projectService.js
+++ frontend/src/features/Projects/projectService.js
@@ -1,71 +1,65 @@
 import axios from 'axios'
 
 const API_URL = 'https://scrum-flow.onrender.com/api';
-const project_URL = `${API_URL}/projects/`;
+const PROJECT_URL = `${API_URL}/projects/`;
 
-router.get('/',protect, getAllProjects);
-router.get('/:id',protect, getProjectById);
-router.put('/:id',protect, updateProject);
-router.put('/:id/assign', protect, assignProject);
-router.delete('/:id',protect, deleteProject);
-
-const createProject = async (formData,token) => {
+const createProject = async (projectData,token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     }
-    const response = await axios.post(POSTS_URL,formData,config)
+    const response = await axios.post(PROJECT_URL,projectData,config)
     return response.data
 }
 // router.get('/',protect, getAllProjects);
-const getAllProjects = async (formData,token) => {
+const getAllProjects = async (token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     }
-    const response = await axios.post(POSTS_URL,formData,config)
+    const response = await axios.get(PROJECT_URL,config)
     return response.data
 }
 // router.get('/:id',protect, getProjectById);
-const getProjectById = async (formData,token) => {
+const getProjectById = async (project_id,token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     }
-    const response = await axios.post(POSTS_URL,formData,config)
+    const response = await axios.get(PROJECT_URL+project_id,config)
     return response.data
 }
 // router.put('/:id',protect, updateProject);
-const updateProject = async (formData,token) => {
+const updateProject = async (projectData,project_id,token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     }
-    const response = await axios.post(POSTS_URL,formData,config)
+    const response = await axios.put(PROJECT_URL+project_id,projectData,config)
     return response.data
 }
 // router.put('/:id/assign', protect, assignProject);
-const assignProject = async (formData,token) => {
+const assignProject = async (projectData,project_id,token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     }
-    const response = await axios.post(POSTS_URL,formData,config)
+    const response = await axios.put(`${PROJECT_URL}${project_id}/assign}`,projectData,config)
     return response.data
 }
 // router.delete('/:id',protect, deleteProject);
-const deleteProject = async (formData,token) => {
+const deleteProject = async (projectData,project_id,token) => {
     const config = {
         headers: {
             Authorization: `Bearer ${token}`,
         },
     }
-    const response = await axios.post(POSTS_URL,formData,config)
+    const response = await axios.delete(PROJECT_URL+project_id,projectData,config)
     return response.data
 }
 
