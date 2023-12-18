78aca416b8d6ab727bb5980c0448837f14f76398
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-18 13:19:59 +0300

    finish users List

--- frontend/src/components/Admin/UserList.jsx
+++ frontend/src/components/Admin/UserList.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllUsers, reset } from '../../features/users/userSlice'; 
-import { FaSpinner, FaEdit } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllUsers, reset } from "../../features/users/userSlice";
+import { FaSpinner, FaEdit,FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const UsersList = () => {
   const dispatch = useDispatch();
-  const { users, isLoading, isError, isSuccess } = useSelector((state) => state.users);
+  const { users, isLoading, isError, isSuccess } = useSelector(
+    (state) => state.users
+  );
 
   useEffect(() => {
     // Dispatch action to get all users
@@ -16,93 +18,127 @@ const UsersList = () => {
     };
   }, [dispatch]);
 
-  // Function to group users by their role
-  const groupUsersByRole = (userList) => {
-    const groupedUsers = {};
-    userList.forEach((user) => {
-      const { role } = user;
-      if (!groupedUsers[role]) {
-        groupedUsers[role] = [];
-      }
-      groupedUsers[role].push(user);
-    });
-    return groupedUsers;
-  };
-
   // handle date formatting
   const handleDate = (dateInput) => {
     const date = new Date(dateInput);
     const formattedDate = date.toISOString().split("T")[0];
     return formattedDate;
   };
-  // Sort user by role and exlude admins from the list
-  const sortUsersByName = (userList) => {
-    const filteredUsers = userList.filter(user => user.role !== 'admin');
-    return [...filteredUsers].sort((a, b) => a.username.localeCompare(b.username));
-  };
 
+  const [selectedRole, setSelectedRole] = useState("Development Team");
+  const handleRole = (role) => {
+    setSelectedRole(role);
+  };
+  const developmentTeamUsers = users.filter(
+    (user) => user.role === "Development Team"
+  );
+  const projectOwnerUsers = users.filter(
+    (user) => user.role === "Product Owner"
+  );
+  const scrumMasterUsers = users.filter((user) => user.role === "Scrum Master");
   // displaying grouped and sorted users
   const renderUsers = () => {
+    let filteredUsers = [];
+    switch (selectedRole) {
+      case "Development Team":
+        filteredUsers = developmentTeamUsers;
+        break;
+      case "Product Owner":
+        filteredUsers = projectOwnerUsers;
+        break;
+      case "Scrum Master":
+        filteredUsers = scrumMasterUsers;
+        break;
+      default:
+        filteredUsers = users;
+        break;
+    }
+    // Sort users alphabetically by username
+    filteredUsers.sort((a, b) => a.username.localeCompare(b.username));
+
     if (isLoading) {
       return <FaSpinner />;
     }
 
     if (isError) {
-      return <p>Error loading users.</p>;
+      return toast.error(message);
     }
 
     if (isSuccess) {
-      const sortedUsers = sortUsersByName(users);
-      const groupedUsers = groupUsersByRole(sortedUsers);
-
       return (
-        <div>
-          {Object.keys(groupedUsers).map((role, index) => (
-            <div key={role} className="mb-8">
-              <h2 className="text-2xl mb-2">
-                <Link to={`/usersList/${role}`} className="flex items-center">
-                  <span className="mr-2">{index + 1}.</span>
-                  {role}
-                </Link>
-              </h2>
-              <table className="table-auto border border-collapse border-olive-green">
-                <thead>
-                  <tr className="bg-olive-green text-white">
-                    <th className="border border-olive-green py-2 px-4">No</th>
-                    <th className="border border-olive-green py-2 px-4">Name</th>
-                    <th className="border border-olive-green py-2 px-4">Username</th>
-                    <th className="border border-olive-green py-2 px-4">Email</th>
-                    <th className="border border-olive-green py-2 px-4">Date of Registration</th>
-                    <th className="border border-olive-green py-2 px-4">Last Update</th>
-                    <th className="border border-olive-green py-2 px-4">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {groupedUsers[role].map((user, userIndex) => (
-                    <tr key={user._id} className={userIndex % 2 === 0 ? 'bg-gray-100' : ''}>
-                      <td className="border border-olive-green py-2 px-4">{userIndex + 1}</td>
-                      <td className="border border-olive-green py-2 px-4">
-                        {user.firstName} {user.lastName}
-                      </td>
-                      <td className="border border-olive-green py-2 px-4">{user.username}</td>
-                      <td className="border border-olive-green py-2 px-4">{user.email}</td>
-                      <td className="border border-olive-green py-2 px-4">
-                        {user.createdAt && handleDate(user.createdAt)}
-                      </td>
-                      <td className="border border-olive-green py-2 px-4">
-                        {user.updatedAt && handleDate(user.updatedAt)}
-                      </td>
-                      <td className="border border-olive-green py-2 px-4">
-                        <Link to={`/updateUserProfile/${user._id}`}>
-                          <FaEdit />
-                        </Link>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          ))}
+        <div className="flex flex-col mx-16 gap-2">
+          <div className="flex flex-row">
+            <button
+              onClick={() => handleRole("Development Team")}
+              className="-none text-blue bg-white hover:text-dark-blue hover:underline"
+            >
+              Development Team
+            </button>/
+            <button
+              onClick={() => handleRole("Scrum Master")}
+              className="-none text-blue bg-white hover:text-dark-blue hover:underline"
+            >
+              Scrum Master
+            </button>/
+            <button
+              onClick={() => handleRole("Product Owner")}
+              className="-none text-blue bg-white hover:text-dark-blue hover:underline"
+            >
+              Product Owner
+            </button>
+          </div>
+
+          <table className="table-auto ">
+            <thead>
+              <tr className="bg-olive-green text-white">
+                <th className="text-left p-2 border border-light-gray">No.</th>
+                <th className="text-left p-2 border border-light-gray">Name</th>
+                <th className="text-left p-2 border border-light-gray">Username</th>
+                <th className="text-left p-2 border border-light-gray">Email</th>
+                <th className="text-left p-2 border border-light-gray">
+                  Date of Registration
+                </th>
+                <th className="text-left p-2 border border-light-gray">
+                  Last Update
+                </th>
+                <th className="text-left p-2 border border-light-gray">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredUsers.map((user, userIndex) => (
+                <tr
+                  key={user._id}
+                  className={userIndex % 2 === 0 ? "bg-light-gray md" : "shadow-ms"}
+                >
+                  <td className=" p-2 ">
+                  {user.profileImage ?
+                    (<img src={user.profileImage} alt="profile picture" className="rounded-full w-12 h-12"/>):
+                    ( <FaUserCircle size={24}/>)
+                  }</td>
+                  <td className=" p-2">
+                    {user.firstName} {user.lastName}
+                  </td>
+                  <td className=" p-2">
+                    {user.username}
+                  </td>
+                  <td className=" p-2">
+                    {user.email}
+                  </td>
+                  <td className=" p-2">
+                    {user.createdAt && handleDate(user.createdAt)}
+                  </td>
+                  <td className=" p-2">
+                    {user.updatedAt && handleDate(user.updatedAt)}
+                  </td>
+                  <td className=" p-2">
+                    <Link to={`/updateUserProfile/${user._id}`}>
+                      <FaEdit />
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       );
     }
