7a311ebcaddaab59a0eb802472e3667413f89d9b
FevenSeyfu <fevenseyfu00@gmail.com>
2023-12-22 23:40:24 +0300

    Add component to delete task

--- frontend/src/components/Tasks/DeleteTask.jsx
+++ frontend/src/components/Tasks/DeleteTask.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import  { deleteTask} from '../../features/Tasks/taskSlice'
+import Modal from "react-modal";
+import { toast } from "react-toastify";
+import { FaSpinner } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 
-const DeleteTask = () => {
+Modal.setAppElement("#root");
+
+const DeleteTask = ({taskId,project_id, onClose}) => {
+  const dispatch = useDispatch();
   return (
     <div>DeleteTask</div>
   )
